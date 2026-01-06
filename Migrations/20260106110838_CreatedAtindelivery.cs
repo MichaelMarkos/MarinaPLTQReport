abc2d59d09f1a6261ad62d5e8d58a2c45232bc94
HanyWork <hani.adeb@vetanoia.com>
2026-01-06 13:09:24 +0200

    addmigre

---
+++ Migrations/20260106110838_CreatedAtindelivery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace maria.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedAtindelivery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "DeliveryReport",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "DeliveryReport");
+        }
+    }
+}
